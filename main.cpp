d591d216aeeff050850124c73b8800f1c012e799
Lin <49216781+shuiyuedongtian123@users.noreply.github.com>
2023-02-10 00:19:20 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//main.cpp
+
+#include "DrawGraph.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int read_v(std::string file_name){
+	return 0;
+}
+
+int main() {
+	
+	//DrawGraph d;
+	//d.Start();
+	//d.Create_Node("TEST1");
+	//d.Create_Node("TEST2");
+	//d.Create_Relationship("TEST1", "TEST2", DrawGraph::Graph, "knows");
+	//d.End();
+	//d.Get_Png("./test.png");
+	return 0;
+}
